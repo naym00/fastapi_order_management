8a479e23ea90721093dacc9acbadf2fad52f273b
naym00 <naymhsain00@gmail.com>
2025-03-25 15:05:51 +0600

    fastapi order management

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from typing import Union
+from fastapi import FastAPI
+from app import schemas as SCHEMA
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: SCHEMA.Item):
+    return {"item_name": item.name, "item_id": item_id}
